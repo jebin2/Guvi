ca2eee3498fb4d8069d5744b7a78b55af59a2300
jebin <jebineinstein@gmail.com>
2017-09-03 12:50:12 +0530

    error

--- NRegisterNumber.java
+++ NRegisterNumber.java
@@ -7,11 +7,13 @@ public class NRegisterNumber {
 		Scanner sc = new Scanner(System.in);
 		HashMap<Integer, Integer> hm1 = new HashMap<Integer, Integer>();
 		HashMap<Integer, Integer> hm2 = new HashMap<Integer, Integer>();
+		System.out.println("Enter the size of an array");
 		int n = sc.nextInt();
 		if (n <= 0) {
 			System.out.println(0);
 		} else {
 			int key, k = 0;
+			System.out.println("Enter the number one by one");
 			for (int i = 0; i < n; i++) {
 				key = sc.nextInt();
 				if (hm1.get(key) == null) {
@@ -20,6 +22,7 @@ public class NRegisterNumber {
 					hm2.put(key, k++);
 				}
 			}
+			System.out.println("The repeated numbers are :");
 			for (Map.Entry<Integer, Integer> m : hm2.entrySet()) {
 				System.out.println(m.getKey());
 			}
